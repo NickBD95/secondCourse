223b35240bb0d183aa2fd2f14f2c1e0288cf1acb
Nickolai Grachev <nickbd95@gmail.com>
2023-09-19 10:48:16 +0300

    Initial Commit

---
+++ secondCourse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  secondCourse
+//
+//  Created by Nikolai Grachev on 19.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
